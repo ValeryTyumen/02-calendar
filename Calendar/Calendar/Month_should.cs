24c407b85701be954d751ddf6635cfe91c236f5a
Валерий <v.lihosherstov@gmail.com>
2014-11-30 21:11:38 +0500

    Tests and Refactoring

---
+++ Calendar/Calendar/Month_should.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Calendar
+{
+    [TestFixture]
+    class Month_should
+    {
+        private Month Month;
+
+        [SetUp]
+        public void SetUpMonth()
+        {
+            var day = new DateTime(2014, 11, 30);
+            Month = new Month(day);
+        }
+
+        [Test]
+        public void determine_first_week()
+        {
+            Assert.AreEqual(44, Month.FirstWeek);
+        }
+
+        [Test]
+        public void determine_current_day()
+        {
+            Assert.AreEqual(Tuple.Create(5, 0), Month.CurrentDay);
+        }
+
+        [Test]
+        public void determine_first_day()
+        {
+            Assert.AreEqual(1, Month.Weeks[0][6]);
+        }
+
+        [Test]
+        public void determine_last_day()
+        {
+            Assert.AreEqual(30, Month.Weeks[5][0]);
+        }
+    }
+}
--- Calendar/Calendar/Program.cs
+++ Calendar/Calendar/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Globalization;
 using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Calendar
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var datetime = DateTime.ParseExact(args[0], "dd.MM.yyyy", CultureInfo.InvariantCulture);
             var calendar = CultureInfo.InvariantCulture.Calendar;
@@ -26,21 +28,43 @@ namespace Calendar
         }
     }
 
-    internal class CalendarPoint
+    internal class Month
     {
-        private PointF point;
-        public float X
+        public string Title { get; private set; }
+        public List<int[]> Weeks { get; private set; }
+        public Tuple<int, int> CurrentDay { get; private set; }
+        public int FirstWeek { get; private set; }
+
+        public Month(DateTime day)
         {
-            get { return point.X; }
-        }
-        public float Y
-        {
-            get { return point.Y; }
+            Title = day.ToString("MMMM yyyy");
+            FirstWeek = GetWeekOfYear(new DateTime(day.Year, day.Month, 1));
+            CurrentDay = Tuple.Create(GetWeekOfYear(day) - FirstWeek, (int)day.DayOfWeek);
+            Weeks = new List<int[]>();
+            for (var i = 1; i <= DateTime.DaysInMonth(day.Year, day.Month); i++)
+            {
+                var dayOfMonth = new DateTime(day.Year, day.Month, i);
+                AddDay(dayOfMonth);
+            }
         }
 
-        public CalendarPoint(float x, float y)
+        public IEnumerable<int> GetWeekNumbers()
         {
-            point = new PointF(x, y);
+            return Enumerable.Range(0, Weeks.Count).Select(z => z + FirstWeek);
+        }
+
+        private void AddDay(DateTime day)
+        {
+            var week = GetWeekOfYear(day) - FirstWeek;
+            if (week >= Weeks.Count)
+                Weeks.Add(new int[7]);
+            Weeks[week][(int) day.DayOfWeek] = day.Day;
+        }
+
+        private int GetWeekOfYear(DateTime day)
+        {
+            var firstDayOfYear = new DateTime(day.Year, 1, 1);
+            return (day.DayOfYear + (int)firstDayOfYear.DayOfWeek - 1) / 7 + 1;
         }
     }
 
@@ -49,121 +73,92 @@ namespace Calendar
         private Size ContextSize;
         private const string FontName = "Segoe WP";
         private Graphics Graphics;
-        private DateTime Day;
+        private Month Month; 
 
         public CalendarPainter(Size contextSize, Graphics graphics, DateTime day)
         {
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             ContextSize = contextSize;
             Graphics = graphics;
-            Day = day;
+            Month = new Month(day);
         }
 
-        private PointF GetContextCoords(CalendarPoint point)
+        public PointF MapGrid(PointF locationOnGrid)
         {
-            var x = ContextSize.Width*point.X/8;
-            var y = ContextSize.Height*point.Y/8;
-            return new PointF(x, y);
+            return new PointF((locationOnGrid.X + 0.5f)*ContextSize.Width/8f, 
+                (locationOnGrid.Y + 0.5f)*ContextSize.Height/8f);
         }
 
-        private void DrawText(string text, Font font, Color color, CalendarPoint cPoint)
+        private void DrawText(string text, Font font, Color color, PointF locationOnGrid)
         {
-            var point = GetContextCoords(cPoint);
             var stringFormat = new StringFormat();
             stringFormat.Alignment = StringAlignment.Center;
             stringFormat.LineAlignment = StringAlignment.Center;
-            Graphics.DrawString(text, font, new SolidBrush(color), point, stringFormat);
+            Graphics.DrawString(text, font, new SolidBrush(color), MapGrid(locationOnGrid), stringFormat);
         }
 
-        private int GetWeekOfYear(DateTime day)
+        private void SelectDay(Month month)
         {
-            var firstDayOfYear = new DateTime(day.Year, 1, 1);
-            return (day.DayOfYear + (int)firstDayOfYear.DayOfWeek - 1)/7 + 1;
-        }
-
-        private DateTime GetFirstDayOfMonth(DateTime day)
-        {
-            var firstDay = new DateTime(day.Year, day.Month, 1);
-            return firstDay;
-        }
-
-        private DateTime GetLastDayOfMonth(DateTime day)
-        {
-            var lastDay = new DateTime(day.Year, day.Month, DateTime.DaysInMonth(day.Year, day.Month));
-            return lastDay;
-        }
-
-        private CalendarPoint GetCalendarPoint(DateTime day)
-        {
-            var firstDayOfMonth = new DateTime(day.Year, day.Month, 1);
-            var firstWeek = GetWeekOfYear(firstDayOfMonth);
-            var week = GetWeekOfYear(day);
-            return new CalendarPoint((float)day.DayOfWeek + 1.5f, week - firstWeek + 2.5f);
-        }
-
-        private void DrawSelection(DateTime day)
-        {
-            var center = GetContextCoords(GetCalendarPoint(day));
+            var center = MapGrid(new PointF(month.CurrentDay.Item2 + 1,month.CurrentDay.Item1 + 2));
             var radius = Math.Min(ContextSize.Width, ContextSize.Height) /16f;
             var brush = new SolidBrush(Color.PowderBlue);
             Graphics.FillEllipse(brush, center.X - radius, center.Y - radius, radius * 2, radius * 2);
         }
 
-        private void DrawMonth(DateTime day)
+        private void DrawTitle(Month month)
         {
-            var cPoint = new CalendarPoint(4f, 0.5f);
-            var text = day.ToString("MMMM yyyy");
+            var location = new PointF(3.5f, 0f);
             var font = new Font(FontName, 20, FontStyle.Bold);
-            DrawText(text, font, Color.Gray, cPoint);
+            DrawText(month.Title, font, Color.Gray, location);
         }
 
-        private void DrawWeekNumbers(DateTime day)
+        private void DrawWeekNumbers(Month month)
         {
-            var firstWeek = GetWeekOfYear(GetFirstDayOfMonth(day));
-            var lastWeek = GetWeekOfYear(GetLastDayOfMonth(day));
-            for (var i = firstWeek; i <= lastWeek; i++)
+            foreach(var weekNumber in month.GetWeekNumbers())
             {
-                var cPoint = new CalendarPoint(0.5f, i - firstWeek + 2.5f);
+                var locationOnGrid = new PointF(0, weekNumber - month.FirstWeek + 2);
                 var font = new Font(FontName, 30);
-                DrawText(i.ToString(), font, Color.DodgerBlue, cPoint);
+                DrawText(weekNumber.ToString(), font, Color.DodgerBlue, locationOnGrid);
             }
         }
 
-        private void DrawWeeksTitle()
+        private void DrawWeekTitles()
         {
             var day = new DateTime(1, 1, 1);
-            day.AddDays(7 - (int)day.DayOfWeek);
-            var cPoint = new CalendarPoint(0.5f, 1.5f);
-            var font = new Font(FontName, 30);
-            DrawText("#", font, Color.LightCoral, cPoint);
+            day = day.AddDays(7 - (int)day.DayOfWeek);
+            var font = new Font(FontName, 25);
+            var locationOnGrid = new PointF(0, 1);
+            DrawText("#", font, Color.Gray, locationOnGrid);
             for (var i = 0; i < 7; i++)
             {
-                cPoint = new CalendarPoint(cPoint.X + 1f, cPoint.Y);
-                DrawText(day.ToString("ddd").ToUpper(), font, Color.LightCoral, cPoint);
+                locationOnGrid = new PointF(i + 1, 1);
+                DrawText(day.ToString("ddd").ToUpper(), font, Color.Gray, locationOnGrid);
                 day = day.AddDays(1);
             }
         }
 
-        private void DrawDays(DateTime day)
+        private void DrawDays(Month month)
         {
-            for (var i = 1; i <= DateTime.DaysInMonth(day.Year, day.Month); i++)
-            {
-                var monthDay = new DateTime(day.Year, day.Month, i);
-                CalendarPoint cPoint = GetCalendarPoint(monthDay);
-                var color = monthDay.DayOfWeek == DayOfWeek.Sunday ? Color.LightCoral : Color.Gray;
-                var font = new Font(FontName, 30);
-                DrawText(monthDay.Day.ToString(), font, color, cPoint);
-            }
+            for (var i = 0; i < month.Weeks.Count; i++)
+                for (var j = 0; j < 7; j++)
+                    if (month.Weeks[i][j] != 0)
+                    {
+                        var locationOnGrid = new PointF(j + 1, i + 2);
+                        var color = j == 0 ? Color.LightCoral : Color.Gray;
+                        var font = new Font(FontName, 30);
+                        DrawText(month.Weeks[i][j].ToString(), font, color, locationOnGrid);
+                    }
         }
 
         public void Draw()
         {
             var brush = new SolidBrush(Color.WhiteSmoke);
             Graphics.FillRectangle(brush, 0, 0, ContextSize.Width, ContextSize.Height);
-            DrawSelection(Day);
-            DrawMonth(Day);
-            DrawWeeksTitle();
-            DrawWeekNumbers(Day);
-            DrawDays(Day);
+            DrawTitle(Month);
+            DrawWeekTitles();
+            SelectDay(Month);
+            DrawDays(Month);
+            DrawWeekNumbers(Month);
         }
 
     }
